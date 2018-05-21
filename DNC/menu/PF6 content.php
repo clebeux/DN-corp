e15a7e56ded0e73df77db04c8a09dbf32c1a7769
Chomp <clebeux@estran-brest.education>
2018-05-21 21:50:18 +0200

    Fiche PF6
    Creation PF6:
    	? SQL
    	? Liste
    	+ CSS
    	+ PHP

---
+++ DNC/menu/PF6 content.php
@@ -0,0 +1,16 @@
+<menu id='menu'>
+	<menu class='top'>inscription aux formations</menu>
+	<menu class='content'>
+		<div class="droite border-left">
+    		<menu class='titre'>khj</menu>
+    		<div class='info text-center'>Accès à la fonction Equipier Incendie FDF</div>
+    		<div class='info text-center'>Personnels inscrits</div>
+    		<input type='submit'>
+    		<input type='button' value='Annuler'>
+    	</div>
+    	<div class="gauche">
+    		<span>Formation: </span><select></select>
+    	</div>
+	</menu>
+
+</menu>
---
+++ DNC/menu/PF6.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+    	<?php 	
+    			include "../lib/php/test_head.php";
+    	?>
+    	<link href="../lib/css/PF1.css" rel="stylesheet" type="text/css" />
+    	
+    </head>
+
+
+    <body>
+        <div id="container">
+        	<?php 
+        		
+        		include "../lib/php/top.php";
+        		
+        	?>
+        <div id="nav"></div>
+        <div>
+        	<div id="content">
+        		<?php 
+        		include "PF6 content.php";
+        		?>
+        	</div>
+        </div>
+        
+        	<?php 
+        		
+        		include "../lib/php/footer.php";
+        	?>
+        </div>
+    </body>
+
+</html>
